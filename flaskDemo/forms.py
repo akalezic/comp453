0c01ab8a2b136702ffcf67aa9f293a49ff05107c
Justin Atteberry <justin_atteberry@yahoo.com>
2022-04-29 18:28:17 -0500

    Added add_item form

--- flaskDemo/forms.py
+++ flaskDemo/forms.py
@@ -5,132 +5,29 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextA
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError,Regexp
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from flaskDemo import db
-from flaskDemo.models import User, Department, getDepartment, getDepartmentFactory, Employee, Project, Works_On
+from flaskDemo.models import Item, Project
 from wtforms.fields.html5 import DateField
-
-ssns = Department.query.with_entities(Department.mgr_ssn).distinct()
-#  or could have used ssns = db.session.query(Department.mgr_ssn).distinct()
-# for that way, we would have imported db from flaskDemo, see above
-
-myChoices2 = [(row[0],row[0]) for row in ssns]  # change
-results=list()
-for row in ssns:
-    rowDict=row._asdict()
-    results.append(rowDict)
-myChoices = [(row['mgr_ssn'],row['mgr_ssn']) for row in results]
-regex1='^((((19|20)(([02468][048])|([13579][26]))-02-29))|((20[0-9][0-9])|(19[0-9][0-9]))-((((0[1-9])'
-regex2='|(1[0-2]))-((0[1-9])|(1\d)|(2[0-8])))|((((0[13578])|(1[02]))-31)|(((0[1,3-9])|(1[0-2]))-(29|30)))))$'
-regex=regex1 + regex2
-
-emp_ssn = Employee.query.with_entities(Employee.ssn).all()
-empChoices = [(row[0],row[0]) for row in emp_ssn] 
-
-prj_num = Project.query.with_entities(Project.pnumber).all()
-prjChoices = [(row[0],row[0]) for row in prj_num] 
-
-class EmployeeForm(FlaskForm):
-    ssn = SelectField("Employee's SSN", choices = empChoices, validators=[DataRequired()])
-    pnumber = SelectField("Project Number", choices = prjChoices, coerce = int, validators=[DataRequired()])
-    hours = DecimalField("Hours", places = 1, validators=[DataRequired()])
-    submit = SubmitField('Add Employee to Project')
-    
-    def validate_pnumber(self, pnumber):
-        proj = Works_On.query.filter_by(essn=self.ssn.data, pno=self.pnumber.data).first()
-        if proj:
-            raise ValidationError('The employee is already assigned to this project.')
-
-class RegistrationForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
-
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError('That username is taken. Please choose a different one.')
-
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user:
-            raise ValidationError('That email is taken. Please choose a different one.')
-
-
-class LoginForm(FlaskForm):
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
-
-
-class UpdateAccountForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
-    submit = SubmitField('Update')
-
-    def validate_username(self, username):
-        if username.data != current_user.username:
-            user = User.query.filter_by(username=username.data).first()
-            if user:
-                raise ValidationError('That username is taken. Please choose a different one.')
-
-    def validate_email(self, email):
-        if email.data != current_user.email:
-            user = User.query.filter_by(email=email.data).first()
-            if user:
-                raise ValidationError('That email is taken. Please choose a different one.')
-
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
-
-    
-class DeptUpdateForm(FlaskForm):
-
-#    dnumber=IntegerField('Department Number', validators=[DataRequired()])
-    dnumber = HiddenField("")
-
-    dname=StringField('Department Name:', validators=[DataRequired(),Length(max=15)])
-#  Commented out using a text field, validated with a Regexp.  That also works, but a hassle to enter ssn.
-#    mgr_ssn = StringField("Manager's SSN", validators=[DataRequired(),Regexp('^(?!000|666)[0-8][0-9]{2}(?!00)[0-9]{2}(?!0000)[0-9]{4}$', message="Please enter 9 digits for a social security.")])
-
-#  One of many ways to use SelectField or QuerySelectField.  Lots of issues using those fields!!
-    mgr_ssn = SelectField("Manager's SSN", choices=myChoices)  # myChoices defined at top
-    
-# the regexp works, and even gives an error message
-#    mgr_start=DateField("Manager's Start Date:  yyyy-mm-dd",validators=[Regexp(regex)])
-#    mgr_start = DateField("Manager's Start Date")
-
-#    mgr_start=DateField("Manager's Start Date", format='%Y-%m-%d')
-    mgr_start = DateField("Manager's start date:", format='%Y-%m-%d')  # This is using the html5 date picker (imported)
-    submit = SubmitField('Update this department')
-
-
-# got rid of def validate_dnumber
-
-    def validate_dname(self, dname):    # apparently in the company DB, dname is specified as unique
-         dept = Department.query.filter_by(dname=dname.data).first()
-         if dept and (str(dept.dnumber) != str(self.dnumber.data)):
-             raise ValidationError('That department name is already being used. Please choose a different name.')
-
-
-class DeptForm(DeptUpdateForm):
-
-    dnumber=IntegerField('Department Number', validators=[DataRequired()])
-    submit = SubmitField('Add this department')
-
-    def validate_dnumber(self, dnumber):    #because dnumber is primary key and should be unique
-        dept = Department.query.filter_by(dnumber=dnumber.data).first()
-        if dept:
-            raise ValidationError('That department number is taken. Please choose a different one.')
             
-
+class AddItemForm(FlaskForm):
+    item_id = IntegerField("Item ID", validators=[DataRequired()])
+    description = StringField("Description", validators=[DataRequired(), Length(min=1, max=50)])
+    name = StringField("Item Name", validators=[DataRequired(), Length(min=1, max=20)])
+    quantity = DecimalField("Quantity", places = 2, validators=[DataRequired()])
+    unit = StringField("Unit", validators=[Length(max=20)])
+    item_type = StringField("Item Type", validators=[DataRequired(), Length(min=1, max=10)])
+    tool_condition = StringField("Tool Condition", validators=[Length(max=20)])
+    color = StringField("Color", validators=[Length(max=20)])
+    size = StringField("Size", validators=[Length(max=20)])
+    finish = StringField("Finish", validators=[Length(max=20)])
+    shape = StringField("Shape", validators=[Length(max=20)])
+    submit = SubmitField("Add Item")
+    
+    def validate_item_id(self, item_id):
+        id = Item.query.filter_by(item_id=self.item_id.data).first()
+        if id:
+            raise ValidationError("That Item ID is already taken")
+            
+    def validate_name(self, name):
+        desc = Item.query.filter_by(name=self.name.data).first()
+        if desc:
+            raise ValidationError("That name already exists")
